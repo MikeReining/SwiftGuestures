c5160ee275b248ec6f31611254eff66138f8154e
Mike Reining <emailmike@gmail.com>
2015-01-22 12:12:06 -0800

    Added logic to bring monkey home - i.e. have to drag monkey to red square

--- SwiftGuestures/ViewController.swift
+++ SwiftGuestures/ViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var blueSquare: UIView!
     @IBOutlet weak var redSquare: UIView!
 
+    @IBOutlet weak var monkey: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Testing Coordinates
+        var monkeyPosition = monkey.center
+        var redSquarePosition = redSquare.center
+        var redSquareFrame = redSquare.frame
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -24,9 +30,16 @@ class ViewController: UIViewController {
 
     @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
         let translation = recognizer.translationInView(self.view)
+        let position = recognizer.locationInView(self.view)
         recognizer.view!.center = CGPoint(x:recognizer.view!.center.x + translation.x,
             y:recognizer.view!.center.y + translation.y)
         recognizer.setTranslation(CGPointZero, inView: self.view)
+        var monkeyPosition = monkey.center
+        
+        if redSquare.frame.contains(monkeyPosition) {
+            println("Monkey is home")
+        }
+
     }
     
 }
