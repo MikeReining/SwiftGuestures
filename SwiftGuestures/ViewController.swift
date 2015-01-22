96327fde59177e81a669159693ae78a56d090ed3
Mike Reining <emailmike@gmail.com>
2015-01-22 11:01:21 -0800

    Added ability to drag monkey around in view

--- SwiftGuestures/ViewController.swift
+++ SwiftGuestures/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var blueSquare: UIView!
+    @IBOutlet weak var redSquare: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translationInView(self.view)
+        recognizer.view!.center = CGPoint(x:recognizer.view!.center.x + translation.x,
+            y:recognizer.view!.center.y + translation.y)
+        recognizer.setTranslation(CGPointZero, inView: self.view)
+    }
+    
 }
 
